db3c1b1acde9a75ceecae0adf0fa3adc30c0a7f4
Alfrisco <inigo.espinosa.arroquia@gamil.com>
2021-11-05 20:51:26 +0100

    Colisiones y UI

--- PR_ZAXXON Espinosa-Arroquia Inigo/Assets/Scripts/Cubo.cs
+++ PR_ZAXXON Espinosa-Arroquia Inigo/Assets/Scripts/Cubo.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cubo : MonoBehaviour
 {
     public float speed = 2.5f;
     public GameObject Columna;
     private Columna columna;
+    [SerializeField] Text speedText;
+    [SerializeField] Text timeText;
+    Renderer rend;
+
+    private string currentTime;
     public GameObject Nave;
     private Cubo cubo;
     public float mySpeed;
-
     bool alive;
     // Start is called before the first frame update
     void Start()
@@ -19,21 +24,27 @@ public class Cubo : MonoBehaviour
         cubo = Nave.GetComponent<Cubo>();
         mySpeed = cubo.speed;
         alive = true;
+        rend = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-         MoverNave();
-         
-         if (speed < 50f && alive == true)
+        MoverNave();
+
+        if (speed < 50f && alive == true)
         {
             speed = speed + 0.1f;
         }
+        speedText.text = "Velocidad: " + speed + " Kmts/h";
+        currentTime = Time.time.ToString("f2");
+        currentTime = "Time is: " + currentTime + " sec.";
+
+        timeText.text = currentTime;
     }
 
     void MoverNave()
-     {
+    {
         float PosX = transform.position.x;
         float PosY = transform.position.y;
         print(transform.position.x);
@@ -80,6 +91,7 @@ public class Cubo : MonoBehaviour
         {
             Debug.Log("objeto1 ha colisionado con objeto3");
             speed = 0f;
+            rend.enabled = false;
             alive = false;
         }
 
