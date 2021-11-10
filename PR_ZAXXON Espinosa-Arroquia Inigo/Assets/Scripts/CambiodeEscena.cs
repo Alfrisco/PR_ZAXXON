15355ae3e43df8c15b4b1438c6beb2af76a43192
Alfrisco <inigo.espinosa.arroquia@gamil.com>
2021-11-10 21:02:20 +0100

    Menu Principal

---
+++ PR_ZAXXON Espinosa-Arroquia Inigo/Assets/Scripts/CambiodeEscena.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambiodeEscena : MonoBehaviour
+{
+    public void Loadscene(string nombre)
+    {
+        SceneManager.LoadScene(nombre);
+    }
+}
