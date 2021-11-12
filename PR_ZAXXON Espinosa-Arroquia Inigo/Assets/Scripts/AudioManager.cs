939ae71658f36e9c3f4d7cd9d717ee5d46abbfb8
Alfrisco <inigo.espinosa.arroquia@gamil.com>
2021-11-12 21:14:44 +0100

    musica

---
+++ PR_ZAXXON Espinosa-Arroquia Inigo/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioClip menuMusica;
+    public AudioClip juegoMusica;
+    public AudioClip fin;
+
+    public Slider sliderMusic;
+    public static AudioManager instance;
+
+    void Awake()
+    {
+        instance = this;
+        InitializeVolume();
+    }
+
+    private void InitializeVolume()
+    {
+        musicSource.volume = PlayerPrefs.GetFloat("MusicVolume", 1.0f);
+
+        sliderMusic.value = musicSource.volume;
+    }
+
+    public void PlaySong(AudioClip audioClip)
+    {
+        musicSource.clip = audioClip;
+        musicSource.Play();
+    }
+
+    public void OnMusicVolumeUpdate()
+    {
+        musicSource.volume = sliderMusic.value;
+        PlayerPrefs.SetFloat("MusicVolume", musicSource.volume);
+        PlayerPrefs.Save();
+    }
+}
